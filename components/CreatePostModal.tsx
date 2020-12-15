6be0ed6c3bef65525e01a3d19b07f08830f2f5e3
NikVogri <55229210+NikVogri@users.noreply.github.com>
2020-12-15 22:02:07 +0100

    feat: integrated WSYWIG

--- components/CreatePostModal.tsx
+++ components/CreatePostModal.tsx
@@ -1,4 +1,8 @@
 import { Field, Form, Formik, FormikValues } from "formik";
+
+import SunEditor from "suneditor-react";
+import "suneditor/dist/css/suneditor.min.css";
+
 import {
   Button,
   FormControl,
@@ -14,21 +18,23 @@ import {
   Textarea,
   useDisclosure,
 } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as Yup from "yup";
 import useApi from "./hooks/useApi";
 import createToast from "../helpers/toast";
+import { useRouter } from "next/router";
 
 const postValidationSchema = Yup.object().shape({
   title: Yup.string()
     .max(255, "Title can't be longer than 255 characters")
     .required("Title is required"),
-  content: Yup.string().required("Content is required"),
 });
 
 const CreatePostModal = ({ openModal, closeModal }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { loading, postData } = useApi();
+  const [content, setContent] = useState("");
+  const { loading, api } = useApi();
+  const router = useRouter();
 
   useEffect(() => {
     if (!isOpen && openModal) {
@@ -44,19 +50,22 @@ const CreatePostModal = ({ openModal, closeModal }) => {
   };
 
   const handlePostSubmit = async (e: FormikValues) => {
-    const res = await postData(
+    const res = await api(
       `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/posts`,
-      { ...e },
-      true
+      "post",
+      true,
+      { ...e, content }
     );
 
-    if (res.data.success) {
+    if (res.success) {
       onClose();
       createToast(
         "Post created",
         "Your post has been successfully created",
         "success"
       );
+
+      router.push(`/posts/${res.post.slug}`);
     }
   };
 
@@ -102,11 +111,12 @@ const CreatePostModal = ({ openModal, closeModal }) => {
                       isInvalid={form.errors.content && form.touched.content}
                     >
                       <FormLabel>content</FormLabel>
-                      <Textarea
-                        type="content"
-                        required
-                        placeholder="content"
-                        {...field}
+                      <SunEditor
+                        onAudioUpload={() => {}}
+                        onVideoUpload={() => {}}
+                        onImageUpload={() => {}}
+                        name="content"
+                        onChange={(val) => setContent(val)}
                       />
                       <FormErrorMessage>{form.errors.content}</FormErrorMessage>
                     </FormControl>
