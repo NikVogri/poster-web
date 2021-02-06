b3fac30300534b07624e28e9a06dc95ff36ddfec
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-02-06 20:00:30 +0100

    feat: better container component

--- components/partials/Container.tsx
+++ components/partials/Container.tsx
@@ -1,9 +1,9 @@
 import { Container as ContainerWrapper } from "@chakra-ui/react";
 import React from "react";
 
-const Container = ({ children }) => {
+const Container = ({ children, ...props }) => {
   return (
-    <ContainerWrapper centerContent={false} maxW="1024px" py="15px">
+    <ContainerWrapper centerContent={false} maxW="1024px" py="15px" {...props}>
       {children}
     </ContainerWrapper>
   );
