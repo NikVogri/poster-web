0e34b447a8c0658b538b6e2eb3e0efeee121af61
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-02-08 19:52:19 +0100

    feat: more distinctive request

--- components/hooks/useEditorSave.ts
+++ components/hooks/useEditorSave.ts
@@ -20,7 +20,7 @@ const useEditorSave = (editorState) => {
       `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/pages/${slug}`,
       "put",
       true,
-      { data: jsonData }
+      { data: jsonData, type: "notebook" }
     );
 
     if (res?.success) {
