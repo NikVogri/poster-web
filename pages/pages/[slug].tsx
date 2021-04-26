637e51893d1b9821d3de887950cd0bbc0c761047
NikVogrinecMotiviti <55229210+NikVogri@users.noreply.github.com>
2021-04-26 23:38:13 +0200

    feat: single page format & now using id instead of slugs

--- pages/pages/[slug].tsx
+++ pages/pages/[slug].tsx
@@ -12,63 +12,61 @@ import Notebook from "../../components/editor/Notebook";
 import PageError from "../../components/page/PageError";
 
 const RootPage = ({ user }) => {
-  const [pageData, setPageData] = useState<Page | null>(null);
-  const router = useRouter();
-  const { api, loading } = useApi();
+	const [pageData, setPageData] = useState<Page | null>(null);
+	const router = useRouter();
+	const { api, loading } = useApi();
 
-  useEffect(() => {
-    if (router.query.slug) {
-      getPageData();
-    }
-  }, [router.query.slug]);
+	useEffect(() => {
+		if (router.query.slug) {
+			getPageData();
+		}
+	}, [router.query.slug]);
 
-  const getPageData = async () => {
-    const { slug } = router.query;
-    const data = await api(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/pages/${slug}`,
-      "get",
-      true
-    );
+	const getPageData = async () => {
+		const { slug } = router.query;
+		const data = await api(
+			`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/pages/${slug}`,
+			"get",
+			true
+		);
 
-    if (data.page) {
-      setPageData(data.page);
-    } else {
-      router.back();
-    }
-  };
+		if (data.page) {
+			setPageData(data.page);
+		} else {
+			router.back();
+		}
+	};
 
-  if (!pageData && !loading) {
-    return <PageError />;
-  }
+	if (!pageData && !loading) {
+		return <PageError />;
+	}
 
-  if (loading) {
-    return <p>Page loading...</p>;
-  }
+	if (loading) {
+		return <p>Page loading...</p>;
+	}
 
-  let editor: JSX.Element;
-  switch (pageData.type) {
-    case "notebook":
-      editor = <Notebook data={pageData} />;
-      break;
-    case "todo":
-      editor = <Todo data={pageData} />;
-      break;
-  }
+	let editor: JSX.Element;
+	switch (pageData.type) {
+		case "notebook":
+			editor = <Notebook data={pageData} />;
+			break;
+		case "todo":
+			editor = <Todo data={pageData} />;
+			break;
+	}
 
-  console.log(pageData);
-
-  return (
-    <Box display="flex" minH="calc(100vh - 71px)">
-      <PageSidebarLeft />
-      {editor}
-      <PageSidebarRight
-        members={pageData.members}
-        pageSlug={pageData.slug}
-        isOwner={user.id === pageData.owner.id}
-        pageOwner={pageData.owner}
-      />
-    </Box>
-  );
+	return (
+		<Box display="flex" minH="calc(100vh - 71px)">
+			<PageSidebarLeft />
+			{editor}
+			<PageSidebarRight
+				members={pageData.members}
+				pageSlug={pageData.slug}
+				isOwner={user.id === pageData.owner.id}
+				pageOwner={pageData.owner}
+			/>
+		</Box>
+	);
 };
 
 export default withAuthentication(RootPage);
