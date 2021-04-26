059a608a8d4cebe12e0e882eee8a1ba797b80c27
NikVogrinecMotiviti <55229210+NikVogri@users.noreply.github.com>
2021-04-26 23:33:50 +0200

    feat: format & fixes correct promise function types

--- components/context/AuthContext.tsx
+++ components/context/AuthContext.tsx
@@ -4,131 +4,130 @@ import createToast from "../../helpers/toast";
 import useApi from "../hooks/useApi";
 
 interface User {
-  username: string;
-  email: string;
-  id: number;
+	username: string;
+	email: string;
+	id: number;
 }
 
 interface AuthContextInterface {
-  user: User;
-  logout: () => void;
-  login: (email: string, password: string) => void;
-  forgotPassword: (email: string) => void;
-  resetPassword: (
-    password: string,
-    token: string
-  ) => Promise<Response | boolean>;
-  setAuthenticatedUser: (user: User) => void;
-  userLoading: boolean;
+	user: User;
+	logout: () => void;
+	login: (email: string, password: string) => Promise<void>;
+	forgotPassword: (email: string) => Promise<void>;
+	resetPassword: (
+		password: string,
+		token: string
+	) => Promise<Response | boolean>;
+	setAuthenticatedUser: (user: User) => void;
+	userLoading: boolean;
 }
 
 const URL = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth`;
 
 export const AuthContext = createContext<AuthContextInterface>({
-  user: { username: "", email: "", id: 0 },
-  logout: () => {},
-  login: () => {},
-  forgotPassword: () => {},
-  resetPassword: async (password: string, token: string) => false,
-  setAuthenticatedUser: (user: User) => {},
-  userLoading: false,
+	user: { username: "", email: "", id: 0 },
+	logout: () => {},
+	login: async () => {},
+	forgotPassword: async () => {},
+	resetPassword: async (password: string, token: string) => false,
+	setAuthenticatedUser: (user: User) => {},
+	userLoading: false,
 });
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState<User>(null);
-  const { api, loading: apiRequestLoading } = useApi();
+	const [user, setUser] = useState<User>(null);
+	const { api, loading: apiRequestLoading } = useApi();
 
-  const router = useRouter();
+	const router = useRouter();
 
-  const setAuthenticatedUser = (user: User) => {
-    setUser(user);
-  };
+	const setAuthenticatedUser = (user: User) => {
+		setUser(user);
+	};
 
-  const login = async (email: string, password: string) => {
-    const data = {
-      email,
-      password,
-    };
+	const login = async (email: string, password: string) => {
+		const data = {
+			email,
+			password,
+		};
 
-    const user = await api(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/login`,
-      "post",
-      true,
-      data
-    );
+		const user = await api(
+			`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/login`,
+			"post",
+			true,
+			data
+		);
 
-    if (user) {
-      setUser(user.user);
-      createToast(
-        "Authentication successful",
-        "User is successfully logged in",
-        "success"
-      );
-      router.push("/");
-    }
-  };
+		if (user) {
+			setUser(user.user);
+			createToast(
+				"Authentication successful",
+				"User is successfully logged in",
+				"success"
+			);
+		}
+	};
 
-  const logout = async () => {
-    await api(`${URL}/logout`, "post", true);
+	const logout = async () => {
+		await api(`${URL}/logout`, "post", true);
 
-    setUser(null);
-    createToast("Logged out", "User is successfully logged out", "success");
+		setUser(null);
+		createToast("Logged out", "User is successfully logged out", "success");
 
-    router.push("/");
-  };
+		router.push("/");
+	};
 
-  const forgotPassword = async (email: string) => {
-    const res = await api(`${URL}/forgot-password`, "post", false, {
-      email,
-    });
+	const forgotPassword = async (email: string) => {
+		const res = await api(`${URL}/forgot-password`, "post", false, {
+			email,
+		});
 
-    if (res?.success) {
-      createToast(
-        "Email Sent",
-        "Email has been sent, please also check your spam folder",
-        "success"
-      );
-    }
+		if (res?.success) {
+			createToast(
+				"Email Sent",
+				"Email has been sent, please also check your spam folder",
+				"success"
+			);
+		}
 
-    router.push("/");
-  };
+		router.push("/");
+	};
 
-  const resetPassword = async (
-    password: string,
-    token: string
-  ): Promise<Response | boolean> => {
-    const res = await api(`${URL}/reset-password`, "post", false, {
-      password,
-      token,
-    });
+	const resetPassword = async (
+		password: string,
+		token: string
+	): Promise<Response | boolean> => {
+		const res = await api(`${URL}/reset-password`, "post", false, {
+			password,
+			token,
+		});
 
-    if (res?.success) {
-      createToast(
-        "Password successfully changed!",
-        "Password changed successfully",
-        "success"
-      );
-      return res;
-    }
+		if (res?.success) {
+			createToast(
+				"Password successfully changed!",
+				"Password changed successfully",
+				"success"
+			);
+			return res;
+		}
 
-    return false;
-  };
+		return false;
+	};
 
-  return (
-    <AuthContext.Provider
-      value={{
-        user,
-        logout,
-        login,
-        userLoading: apiRequestLoading,
-        forgotPassword,
-        resetPassword,
-        setAuthenticatedUser,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+	return (
+		<AuthContext.Provider
+			value={{
+				user,
+				logout,
+				login,
+				userLoading: apiRequestLoading,
+				forgotPassword,
+				resetPassword,
+				setAuthenticatedUser,
+			}}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
 };
 
 export default AuthProvider;
