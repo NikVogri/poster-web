0211b228646f52dccfd093e98e1f228c90615927
NikVogrinecMotiviti <55229210+NikVogri@users.noreply.github.com>
2021-04-26 23:36:43 +0200

    feat: new styles & removed chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,49 +1,52 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import AuthProvider from "../components/context/AuthContext";
 import Layout from "../components/partials/Layout";
-import "../styles/globals.css";
+
 import "../styles/editor.css";
 import axios, { AxiosResponse } from "axios";
 import EditorProvider from "../components/context/EditorContext";
 
+import "../styles/main.scss";
+import "../styles/spinner.scss";
+
 function MyApp({ Component, pageProps, user }) {
-  return (
-    <AuthProvider>
-      <ChakraProvider>
-        <Layout currentUser={user}>
-          <EditorProvider>
-            <Component {...pageProps} />
-          </EditorProvider>
-        </Layout>
-      </ChakraProvider>
-    </AuthProvider>
-  );
+	return (
+		<AuthProvider>
+			{/* <ChakraProvider> */}
+				<Layout currentUser={user}>
+					<EditorProvider>
+						<Component {...pageProps} />
+					</EditorProvider>
+				</Layout>
+			{/* </ChakraProvider> */}
+		</AuthProvider>
+	);
 }
 
 MyApp.getInitialProps = async ({ ctx }) => {
-  if (!ctx.req || !ctx.req.headers.cookie) {
-    return {};
-  }
+	if (!ctx.req || !ctx.req.headers.cookie) {
+		return {};
+	}
 
-  let user = null;
-  let res: AxiosResponse;
+	let user = null;
+	let res: AxiosResponse;
 
-  try {
-    res = await axios.get(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/me`,
-      {
-        withCredentials: true,
-        headers: {
-          cookie: ctx.req.headers?.cookie,
-        },
-      }
-    );
-    user = res.data.user;
-  } catch (err) {}
+	try {
+		res = await axios.get(
+			`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/me`,
+			{
+				withCredentials: true,
+				headers: {
+					cookie: ctx.req.headers?.cookie,
+				},
+			}
+		);
+		user = res.data.user;
+	} catch (err) {}
 
-  return {
-    user,
-  };
+	return {
+		user,
+	};
 };
 
 export default MyApp;
