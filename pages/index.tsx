d3963df66d4b993f57622719cc120f7344524aee
NikVogrinecMotiviti <55229210+NikVogri@users.noreply.github.com>
2021-04-26 23:36:53 +0200

    feat: placeholder index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,21 @@
-import { Grid, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import withAuthentication from "../components/hoc/withAuthentication";
-import useApi from "../components/hooks/useApi";
-import PageCard from "../components/PageCard";
-import Container from "../components/partials/Container";
+import { Text } from "@chakra-ui/react";
+import Link from "next/link";
+import Container from "../components/partials/Container/Container";
 
 const Home = () => {
-  const [pages, setPages] = useState([]);
-  const { api } = useApi();
+	return (
+		<Container>
+			<Text>Welcome to Mipage</Text>
+			<Text>Version: alpha 0.0.5</Text>
+			<Text>Developed by Nik Vogrinec</Text>
+			<a href="https://www.github.com/nikvogri">Github</a>
 
-  useEffect(() => {
-    fetchUserPages();
-  }, []);
-
-  const fetchUserPages = async () => {
-    const res = await api(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/pages/all`,
-      "get",
-      true
-    );
-
-    if (res) {
-      setPages(res.pages);
-    }
-  };
-
-  return (
-    <Container>
-      <Text my="30px" fontWeight="bold" fontSize="3xl">
-        My Pages
-      </Text>
-      <Grid gridTemplateColumns="repeat(4, minmax(100px, 1fr))" gap={3}>
-        {pages.map((page) => (
-          <PageCard
-            key={page.id}
-            title={page.title}
-            slug={page.slug || ""}
-            updatedAt={page.updatedAt}
-            type={page.type}
-            isPrivate={page.private}
-          />
-        ))}
-        {pages.length == 0 && <p>No pages found, create one now!</p>}
-      </Grid>
-    </Container>
-  );
+			<hr />
+			<Link href="/pages/new">
+				<a>Create new page</a>
+			</Link>
+		</Container>
+	);
 };
 
-export default withAuthentication(Home);
+export default Home;
