a2cfc14e656e026a58b814641fc451cde0cdff70
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-05-01 00:53:39 +0200

    feat: page settings component

---
+++ components/page/PageSettings/PageSettings.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./PageSettings.module.scss";
+
+const PageSettings: React.FC = ({}) => {
+	return (
+		<div className="card">
+			<h3>settings</h3>
+		</div>
+	);
+};
+
+export default PageSettings;
