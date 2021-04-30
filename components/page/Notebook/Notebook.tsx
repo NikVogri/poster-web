3aab5405421083a42f2c087c85b7981fcc7d867f
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-05-01 00:53:16 +0200

    feat: notebook component

---
+++ components/page/Notebook/Notebook.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./Notebook.module.scss";
+
+const Notebook: React.FC = () => {
+	return (
+		<div className={styles.notebook}>
+			<h3>main</h3>
+		</div>
+	);
+};
+
+export default Notebook;
