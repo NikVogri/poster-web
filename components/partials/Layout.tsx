477c7d7da2430f4815f82f943b60acb88458bb87
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-05-01 00:55:08 +0200

    chores

--- components/partials/Layout.tsx
+++ components/partials/Layout.tsx
@@ -4,18 +4,18 @@ import { AuthContext } from "../context/AuthContext";
 import Navigation from "./Navigation/Navigation";
 
 const Layout = ({ children, currentUser }) => {
-  const { setAuthenticatedUser } = useContext(AuthContext);
+	const { setAuthenticatedUser } = useContext(AuthContext);
 
-  useEffect(() => {
-    setAuthenticatedUser(currentUser);
-  }, []);
+	useEffect(() => {
+		setAuthenticatedUser(currentUser);
+	}, []);
 
-  return (
-    <>
-      <Navigation />
-      <Box position="relative">{children}</Box>
-    </>
-  );
+	return (
+		<>
+			<Navigation />
+			<Box position="relative">{children}</Box>
+		</>
+	);
 };
 
 export default Layout;
