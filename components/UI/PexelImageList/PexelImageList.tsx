2d7fae46969f1733bad5a815ea401c82ca21eefd
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-05-01 00:54:23 +0200

    feat: component that fetches pexel images

---
+++ components/UI/PexelImageList/PexelImageList.tsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
+
+import styles from "./PexelImageList.module.scss";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
+
+interface PexelImageListProps {
+	onImageSelect: (image: Image) => void;
+}
+
+interface Image {
+	tiny: string;
+	landscape: string;
+	id: number;
+	photographer: string;
+}
+
+const PexelImageList: React.FC<PexelImageListProps> = ({ onImageSelect }) => {
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState<null | string>(null);
+	const [imageList, setImageList] = useState<Image[]>([]);
+
+	useEffect(() => {
+		fetchImages();
+	}, []);
+
+	const fetchImages = async () => {
+		try {
+			setLoading(true);
+			setError(null);
+
+			const res = await axios.get(`https://api.pexels.com/v1/curated`, {
+				headers: {
+					Authorization:
+						"Bearer " + process.env.NEXT_PUBLIC_PEXELS_KEY,
+				},
+			});
+
+			const images = res.data.photos.map((image: any) => ({
+				landscape: image.src.landscape,
+				tiny: image.src.tiny,
+				id: image.id,
+				photographer: image.photographer,
+			}));
+			setImageList(images);
+		} catch (err) {
+			setError("Could not load images");
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	if (error) {
+		return (
+			<div className={styles.image__list}>
+				<div className={styles.main__container}>
+					<p>{error}</p>
+					<button onClick={() => fetchImages()}>Retry</button>
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<div className={styles.image__list}>
+			{loading ? (
+				<div className={styles.main__container}>
+					<LoadingSpinner size="xlg" />
+				</div>
+			) : (
+				<ul className={styles.image__container}>
+					{imageList.map((image: Image) => (
+						<li key={image.id}>
+							<button onClick={() => onImageSelect(image)}>
+								<img
+									src={image.tiny}
+									alt={image.photographer}
+								/>
+							</button>
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
+};
+
+export default PexelImageList;
