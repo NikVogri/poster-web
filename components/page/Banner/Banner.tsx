423a7214fe0b8a4203eae253a561820553fc9340
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-05-02 18:56:07 +0200

    feat: banner can now be changed on each todo page & each notebook

--- components/page/Banner/Banner.tsx
+++ components/page/Banner/Banner.tsx
@@ -1,6 +1,12 @@
-import React, { useState } from "react";
+import { useRouter } from "next/router";
+import React, { useContext, useEffect, useState } from "react";
+import { Image } from "../../../interfaces/image";
+import { PageType } from "../../../interfaces/page";
+import { PageContext } from "../../context/PageContext";
+import useApi from "../../hooks/useApi";
 
 import ImageListModal from "../../modals/ImageListModal/ImageListModal";
+import LoadingSpinner from "../../UI/LoadingSpinner/LoadingSpinner";
 import styles from "./Banner.module.scss";
 
 const editSvg = (
@@ -26,27 +32,69 @@ const editSvg = (
 const Banner: React.FC = () => {
 	const [openModal, setOpenModal] = useState(false);
 	const [backgroundImage, setBackgroundImage] = useState(null);
+	const { page, notebook } = useContext(PageContext);
+	const { api, loading } = useApi();
 
-	const handleBannerSelect = async (image) => {
+	useEffect(() => {
+		console.log(page);
+		if (page.type === PageType.Notebook) {
+			if (notebook.banner.url) {
+				setBackgroundImage(notebook.banner.url);
+			}
+		} else if (page.type === PageType.Todo) {
+			if (page.banner.url) {
+				setBackgroundImage(page.banner.url);
+			}
+		}
+	}, [page]);
+
+	const handleBannerSelect = async (image: Image) => {
 		setBackgroundImage(image.landscape);
 		setOpenModal(false);
 
 		// TODO: save banner decision to BE
+		const params = {
+			url: image.landscape,
+		};
+
+		if (page.type === PageType.Notebook) {
+			params["notebookId"] = notebook.id;
+		}
+
+		await api(
+			`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/pages/${page.id}/update-banner`,
+			"put",
+			true,
+			params
+		);
 	};
 
 	return (
 		<>
-			<div
-				className={styles.page__banner}
-				style={{ backgroundImage: `url(${backgroundImage})` }}
-			>
-				<button
-					className={styles.edit}
-					onClick={() => setOpenModal(true)}
+			{loading ? (
+				<div
+					className={`${styles.page__banner} ${
+						loading ? styles.loading : ""
+					}`}
+					style={{
+						backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.75),  rgba(0, 0, 0, 0.75)), url(${backgroundImage})`,
+					}}
 				>
-					{editSvg}
-				</button>
-			</div>
+					<LoadingSpinner size="md" />
+				</div>
+			) : (
+				<div
+					className={`${styles.page__banner} ${loading}`}
+					style={{ backgroundImage: `url(${backgroundImage})` }}
+				>
+					<button
+						className={styles.edit}
+						onClick={() => setOpenModal(true)}
+					>
+						{editSvg}
+					</button>
+				</div>
+			)}
 			{openModal && (
 				<ImageListModal
 					handleImageSelect={handleBannerSelect}
