0b4ca83d8f71a253b2fdc0db5210e6ad3c4f3a16
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-05-02 18:56:19 +0200

    feat: now shows spinner if page content is loading

--- components/page/PageCenter/PageCenter.tsx
+++ components/page/PageCenter/PageCenter.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PageType } from "../../../interfaces/page";
+import { PageContext } from "../../context/PageContext";
 import Banner from "../Banner/Banner";
 import Notebook from "../Notebook/Notebook";
+import Todo from "../Todo/Todo";
 
 import styles from "./PageCenter.module.scss";
 
 export const PageCenter: React.FC = () => {
+	const { page } = useContext(PageContext);
+
 	return (
 		<main className={`${styles.page__center} card`}>
-			<Banner />
+			{page.banner.active && <Banner />}
 			<Notebook />
+			{page.type === PageType.Todo && <Todo />}
+			{page.type === PageType.Notebook && <Notebook />}
 		</main>
 	);
 };
