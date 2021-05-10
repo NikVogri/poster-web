9a287f8141dba9fecf90dad296e74a35c8a0bd12
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-05-11 00:22:41 +0200

    feat: base modal improvements

--- components/UI/BaseModal/BaseModal.tsx
+++ components/UI/BaseModal/BaseModal.tsx
@@ -1,30 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Portal from "../../Portal/Portal";
 
 import styles from "./BaseModal.module.scss";
 
+import { createPortal } from "react-dom";
 interface BaseModalInterface {
 	children: any;
 	open: boolean;
 	toggleOpen: (status: boolean) => void;
+	footer?: JSX.Element;
+	header?: JSX.Element;
 }
 
 const BaseModal: React.FC<BaseModalInterface> = ({
 	children,
 	open,
-	toggleOpen,
+	header,
+	footer,
 }) => {
-	return (
-		<Portal>
-			<div
-				className={`${styles.base__modal} ${
-					open ? styles.open : styles.closed
-				}`}
-			>
-				{children}
-				<button onClick={() => toggleOpen(false)}>close</button>
-			</div>
-		</Portal>
+	if (!open) return <></>;
+
+	return createPortal(
+		<dialog
+			open={open}
+			className={`${styles.base__modal} ${
+				open ? styles.open : styles.closed
+			}`}
+		>
+			{header && <div className={styles.modal__header}>{header}</div>}
+			<div className={styles.modal__content}>{children}</div>
+			{footer && <div className={styles.modal__footer}>{footer}</div>}
+		</dialog>,
+		document.getElementById("modal")
 	);
 };
 
