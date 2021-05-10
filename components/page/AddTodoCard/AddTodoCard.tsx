5f0e6fc4c4e2c2c123d6563471075e38375f0d5f
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-05-11 00:23:29 +0200

    feat: add todo card

---
+++ components/page/AddTodoCard/AddTodoCard.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import CreateTodoCardModal from "../../modals/CreateTodoCardModal/CreateTodoCardModal";
+
+import styles from "./AddTodoCard.module.scss";
+
+const AddTodoCard: React.FC = () => {
+	const [modalOpen, setModalOpen] = useState(false);
+
+	return (
+		<>
+			<button
+				className={styles.todo__card}
+				title="Add a new todo list"
+				onClick={() => setModalOpen(true)}
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="22"
+					height="22"
+					viewBox="0 0 448 512"
+				>
+					<g>
+						<path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"></path>
+					</g>
+				</svg>
+			</button>
+			<CreateTodoCardModal
+				openModal={modalOpen}
+				setOpenModal={setModalOpen}
+			/>
+		</>
+	);
+};
+
+export default AddTodoCard;
