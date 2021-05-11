061f00d232d90289db18a0b9011b81d0087999ed
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-05-11 22:18:55 +0200

    feat: modal can now have a load state

--- components/UI/BaseModal/BaseModal.tsx
+++ components/UI/BaseModal/BaseModal.tsx
@@ -4,12 +4,14 @@ import Portal from "../../Portal/Portal";
 import styles from "./BaseModal.module.scss";
 
 import { createPortal } from "react-dom";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 interface BaseModalInterface {
 	children: any;
 	open: boolean;
 	toggleOpen: (status: boolean) => void;
 	footer?: JSX.Element;
 	header?: JSX.Element;
+	loading?: boolean;
 }
 
 const BaseModal: React.FC<BaseModalInterface> = ({
@@ -17,6 +19,7 @@ const BaseModal: React.FC<BaseModalInterface> = ({
 	open,
 	header,
 	footer,
+	loading = false,
 }) => {
 	if (!open) return <></>;
 
@@ -25,8 +28,11 @@ const BaseModal: React.FC<BaseModalInterface> = ({
 			open={open}
 			className={`${styles.base__modal} ${
 				open ? styles.open : styles.closed
-			}`}
+			} ${loading ? styles.loading : ""}`}
 		>
+			{loading && (
+				<LoadingSpinner size="lg" className={styles.modal__spinner} />
+			)}
 			{header && <div className={styles.modal__header}>{header}</div>}
 			<div className={styles.modal__content}>{children}</div>
 			{footer && <div className={styles.modal__footer}>{footer}</div>}
