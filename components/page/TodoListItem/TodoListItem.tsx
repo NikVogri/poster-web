7591e64c9f2bc07994a0a9148b47cd340411424b
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-05-11 23:48:01 +0200

    feat: todo list item now updates correctly

--- components/page/TodoListItem/TodoListItem.tsx
+++ components/page/TodoListItem/TodoListItem.tsx
@@ -1,20 +1,51 @@
-import React from "react";
-import { TodoItem } from "../../../interfaces/todo";
+import React, { useContext, useEffect, useState } from "react";
+import { TodoItem, TodoItemUpdate } from "../../../interfaces/todo";
+import { PageContext } from "../../context/PageContext";
+import useApi from "../../hooks/useApi";
 
 import styles from "./TodoListItem.module.scss";
 
 interface TodoListItemProps {
 	todoItem: TodoItem;
+	todoBlockId: string;
 }
 
-const TodoListItem: React.FC<TodoListItemProps> = ({ todoItem }) => {
+const TodoListItem: React.FC<TodoListItemProps> = ({
+	todoItem,
+	todoBlockId,
+}) => {
+	const [task, setTask] = useState<TodoItem>(todoItem);
+	const { page } = useContext(PageContext);
+
+	const { api, loading } = useApi();
+
+	// TODO: loading
+
+	const handleTodoTaskUpdate = async (updateType: TodoItemUpdate) => {
+		const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/pages/${page.id}/todos/${todoBlockId}/task`;
+		const res = await api(url, "put", true, {
+			updateType,
+			taskId: todoItem.id,
+		});
+
+		setTask(res.todoItem);
+	};
+
+	if (!task) return <></>;
+
 	return (
 		<li className={styles.todo__card}>
 			<button
 				className={styles.todo__content}
-				title={`Mark as ${
-					todoItem.completed ? "uncomplete" : "complete"
-				}`}
+				title={`Mark as ${task.completed ? "uncomplete" : "complete"}`}
+				type="button"
+				onClick={() =>
+					handleTodoTaskUpdate(
+						TodoItemUpdate[
+							task.completed ? "UNCOMPLETE" : "COMPLETE"
+						]
+					)
+				}
 			>
 				<svg
 					aria-hidden="true"
@@ -23,17 +54,22 @@ const TodoListItem: React.FC<TodoListItemProps> = ({ todoItem }) => {
 					data-icon="check"
 					className={`svg-inline--fa fa-check fa-w-16 ${
 						styles.checkmark
-					} ${todoItem.completed ? styles.active : ""}`}
+					} ${task.completed ? styles.active : ""}`}
 					role="img"
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 512 512"
 				>
 					<path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path>
 				</svg>
-				<span>{todoItem.text}</span>
+				<span>{task.text}</span>
 			</button>
 
-			<button title="Remove" className={styles.todo__remove}>
+			<button
+				title="Remove"
+				className={styles.todo__remove}
+				type="button"
+				onClick={() => handleTodoTaskUpdate(TodoItemUpdate.REMOVE)}
+			>
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="26"
